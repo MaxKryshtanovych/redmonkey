d928584daf83a820186e4269184c5f4ef5f8a3fe
MaxKryshtanovych <89031940+MaxKryshtanovych@users.noreply.github.com>
2021-10-21 16:53:24 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-                  path('webhook/', csrf_exempt(views.WebHookView.as_view())),
+                  path('/' + settings.TOKEN, views.getMessage, name='gm'),
+                  path('/', views.webhook, name='wh'),
                   path('', views.index, name='index'),
                   path('client/', views.client_list, name='client_list'),
                   path('client/<int:pk>/', views.client_detail, name='client_detail'),
